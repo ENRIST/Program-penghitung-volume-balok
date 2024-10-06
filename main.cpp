94dd20670bc76f7d977d0947dd5269d2ac288cb7
ENRIST <royalglobal09@gmail.com>
2024-10-06 20:46:31 +0700

    make a repo

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int p,l,t,volume;
+    cout << "Kalkulator penghitung volume Balok" <<endl;
+    cout << "==================================" <<endl;
+    cout << "Silahkan masukkan Panjang :";
+    cin >> p;
+    cout << "Silahkan masukkan Lebar :";
+    cin >> l;
+    cout << "Silahkan Masukkan Tinggi :";
+    cin >> t;
+
+    volume = p * l * t;
+
+    cout << "===================================" <<endl;
+    cout << "Hasil dari perhitungan Volume balok dengan panjang " << p << " luas " << l << " dan tinggi " << t << " adalah " << volume;
+
+    return 0;
+}
